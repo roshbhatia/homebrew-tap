d36f235e6a02c705badf6a7e49fc7d1f45d25ec6
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-06 00:25:01 -0700

    chore(orc): publish 0.10.8

--- Formula/orc.rb
+++ Formula/orc.rb
@@ -2,31 +2,31 @@
 class Orc < Formula
   desc "Provider-neutral control plane for agent workflows"
   homepage "https://github.com/roshbhatia/orc"
-  url "https://github.com/roshbhatia/orc/releases/download/v0.10.6/orc_0.10.6_darwin_arm64.tar.gz"
-  sha256 "27951f57354099d51e349e5b54812b1c08588f604b4efb95b216f8b0b8151e89"
+  url "https://github.com/roshbhatia/orc/releases/download/v0.10.8/orc_0.10.8_darwin_arm64.tar.gz"
+  sha256 "234fdb290481eccb5ebdd7710bc0c713881a6413ae4dc4344ae3439e905b37dc"
   license "MIT"
 
   on_macos do
     depends_on arch: :arm64
 
     on_arm do
-      url "https://github.com/roshbhatia/orc/releases/download/v0.10.6/orc_0.10.6_darwin_arm64.tar.gz"
-      sha256 "27951f57354099d51e349e5b54812b1c08588f604b4efb95b216f8b0b8151e89"
+      url "https://github.com/roshbhatia/orc/releases/download/v0.10.8/orc_0.10.8_darwin_arm64.tar.gz"
+      sha256 "234fdb290481eccb5ebdd7710bc0c713881a6413ae4dc4344ae3439e905b37dc"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/roshbhatia/orc/releases/download/v0.10.6/orc_0.10.6_linux_arm64.tar.gz"
-      sha256 "8512256a5351496a9191a9cdd6bcef4356c64affb33d993a14555fe8ff58b785"
+      url "https://github.com/roshbhatia/orc/releases/download/v0.10.8/orc_0.10.8_linux_arm64.tar.gz"
+      sha256 "02807209ddf70d1fafbf0edb10b26d1cab6995bca4e17b77de7afc2f5384bc2e"
     end
     on_intel do
-      url "https://github.com/roshbhatia/orc/releases/download/v0.10.6/orc_0.10.6_linux_amd64.tar.gz"
-      sha256 "df43afbece8e00d9f6c8cad152134be9ecdfa5edfa802573c957c025fecd3d56"
+      url "https://github.com/roshbhatia/orc/releases/download/v0.10.8/orc_0.10.8_linux_amd64.tar.gz"
+      sha256 "2860da19dd59571246f9a81401dfa4c1a75f444288408492b176a4c8fbec6a97"
     end
   end
 
   def install
-    archive_root = Dir["orc_0.10.6_*_*"]
+    archive_root = Dir["orc_0.10.8_*_*"]
                    .find { |path| File.directory?(path) } || buildpath
 
     bin.install "#{archive_root}/bin/orc"
